7c7303022677d5e1290ccc67df652af4e18a30ff
Mattis Stordalen Flister <mattis.stordalen.flister@gmail.com>
2019-03-31 11:59:33 +0200

    Added support for sudo Using try/except instead of signal handlers and poison pills Other changes

--- tcpdumpling.py
+++ tcpdumpling.py
@@ -1,7 +1,9 @@
 import argparse
 import logging
 import multiprocessing
-import signal
+import getpass
+import warnings
+from functools import partial
 
 import paramiko
 
@@ -9,26 +11,35 @@ import paramiko
 class RemoteTcpDump(multiprocessing.Process):
     def __init__(
             self,
+            tcpdump_filter,
             remote_host,
             stdout_pipe,
-            poison_pill,
             username=None,
             password=None,
             pem_file=None,
             ssh_safety=True,
+            sudo=False,
+            echo_only=False,
+            pcap=False,
             log_level=logging.INFO
     ):
         super(RemoteTcpDump, self).__init__()
-
         self.daemon = True
+
         self.remote_host = remote_host
         self.username = username
         self.password = password
         self.stdout_pipe = stdout_pipe
-        self.poison_pill = poison_pill
         self.log_level = log_level
         self.pem_file = pem_file
-        self.ssh_safety = ssh_safety
+        self.pcap = pcap
+
+        self.command = self.build_command(tcpdump_filter, ssh_safety, sudo, echo_only, pcap)
+
+        self.debug(f"Final command: {self.command}")
+
+        if not self.pem_file and not self.password:
+            self.password = getpass.getpass(f"{self.remote_host}: Please enter password: ", )
 
     def connect(self):
         ssh_client = paramiko.SSHClient()
@@ -42,96 +53,144 @@ class RemoteTcpDump(multiprocessing.Process):
         if self.pem_file:
             arguments_to_paramiko['key_filename'] = self.pem_file
 
+        if self.password:
+            arguments_to_paramiko['password'] = self.password
+
         ssh_client.connect(**arguments_to_paramiko)
 
         return ssh_client
 
+    def build_command(self, tcpdump_filter, ssh_safety, sudo, echo_only, pcap):
+        command = f"tcpdump -n"
+
+        if pcap:
+            command = f"{command} -w -"
+
+        command = f"{command} '({tcpdump_filter})'"
+
+        if ssh_safety:
+            command = f"{command} and '(port not 22)'"
+
+        if sudo:
+            command = f'sudo {command}'
+
+        if echo_only:
+            command = f'echo {command}'
+
+        return command
+
+    def debug(self, msg):
+        logging.debug(f"{self.remote_host}: {msg}")
+
+    def info(self, msg):
+        logging.info(f"{self.remote_host}: {msg}")
+
+    def warning(self, msg):
+        logging.warning(f"{self.remote_host}: {msg}")
+
     def run(self):
         logging.basicConfig(level=self.log_level)
 
+        # Paramiko module outputted a lot of deprecation warnings to CLI
+        warnings.filterwarnings(action='ignore', module='.*paramiko.*')
+
         ssh_client = self.connect()
 
-        stdin, stdout, stderr = ssh_client.exec_command('while true; do echo "Hello"; sleep 1; done;')
-        stdin.close()
-        stderr.close()
+        _, stdout, stderr = ssh_client.exec_command(self.command)
 
         while not stdout.channel.exit_status_ready() or stdout.channel.recv_ready():
             try:
-                if stdout.channel.recv_ready():
-                    data = stdout.channel.recv(1024)
-                    while data:
-                        self.stdout_pipe.send_bytes(data)
-                        data = stdout.channel.recv(1024)
+                if self.pcap:
+                    for chunk in stdout.read(1024):
+                        self.stdout_pipe.send_bytes(bytes(chunk))
+                else:
+                    line = b""
+                    # This was a bit tricky, because I want to flush one line at a time to the parent process.
+                    # To trigger on the newline sentinel, I have to read every single byte. Probably slow.. But it works.
+                    for byte in iter(partial(stdout.read, 1), b"\n"):
+                        line += byte
+
+                    self.stdout_pipe.send_bytes(line)
+
             except KeyboardInterrupt:
-                logging.debug(f"{self.remote_host}: Received CTRL+C, exiting")
+                self.info("Received CTRL+C, exiting")
+                ssh_client.close()
                 break
 
-        self.stdout_pipe.close()
+        exit_code = stdout.channel.recv_exit_status()
+
+        if exit_code > 0:
+            self.warning(f"Something happened to the process, exited with non-zero exit code: {exit_code}.")
+            self.warning(f"{stderr.readlines()}")
+
         stdout.close()
-        ssh_client.close()
-
-
-def set_up_logging(log_level):
-    logging.getLogger("paramiko").setLevel(logging.WARNING)
-
-    # Paramiko module outputted a lot of deprecation warnings to CLI
-    import warnings
-    warnings.filterwarnings(action='ignore', module='.*paramiko.*')
-
-    logging.basicConfig(level=log_level)
+        stderr.close()
+        self.stdout_pipe.close()
 
 
 def main(cli_arguments):
     log_level = logging.DEBUG if cli_arguments.debug else logging.INFO
-    set_up_logging(log_level)
+    logging.basicConfig(level=log_level)
     remote_processes = []
 
     for host in cli_arguments.hosts:
-        parent_connection, child_connection = multiprocessing.Pipe()
-        poison_pill = multiprocessing.Event()
+        parent_pipe, child_pipe = multiprocessing.Pipe()
 
         process = RemoteTcpDump(
+            cli_arguments.filter,
             host,
-            child_connection,
-            poison_pill,
+            child_pipe,
             log_level=log_level,
             username=cli_arguments.username,
             pem_file=cli_arguments.pem_file,
+            echo_only=cli_arguments.echo_only,
+            sudo=cli_arguments.sudo,
+            pcap=True if cli_arguments.pcap else False,
         )
 
-        remote_processes.append((process, parent_connection, child_connection, poison_pill))
+        remote_processes.append((process, parent_pipe, child_pipe))
         process.start()
 
-    ctrl_c_received = False
+        # Close it here so that it is closed on both ends
+        child_pipe.close()
 
-    def keyboard_interrupt_handler(signal, frame):
-        logging.debug("Parent: Received CTRL+C, stopping jobs")
-        nonlocal ctrl_c_received
-        ctrl_c_received = True
+    pcap_file = False
+    if cli_arguments.pcap:
+        pcap_file = open(cli_arguments.pcap, "wb")
 
-        for _, _, _, poison_pill in remote_processes:
-            poison_pill.set()
+    while True:
+        try:
+            for process, parent_pipe, _ in remote_processes:
+                if cli_arguments.pcap:
 
-    signal.signal(signal.SIGINT, keyboard_interrupt_handler)
+                    for chunk in parent_pipe.recv_bytes(1024):
+                        pcap_file.write(bytes(chunk))
+                        pcap_file.flush()
+                else:
+                    for line in parent_pipe.recv_bytes().decode("utf-8").split("\n"):
+                        print(f'{process.remote_host}: {line}')
 
-    while not ctrl_c_received:
-        for process, child_stdout, _, _ in remote_processes:
-            if child_stdout.poll():
-                print(f'Parent: got line from {process.remote_host}: {child_stdout.recv_bytes()}')
+        except KeyboardInterrupt:
+            logging.debug("Parent: Received CTRL+C, stopping processing")
+            break
 
-    for process, _, _, _ in remote_processes:
+    for process, _, _ in remote_processes:
         process.join()
+
+    if pcap_file:
+        pcap_file.close()
 
 
 def process_cli_arguments():
     parser = argparse.ArgumentParser(
         description="TCP Dumpling - Connect to multiple hosts and run tcpdump",
-        epilog="Author: Mattis Stordalen Flister (mattis@altibox.no)",
+        epilog="Author: Mattis Stordalen Flister (mattis.stordalen.flister@gmail.com)",
     )
 
     parser.add_argument(
-        'FILTER',
-        help="The TCP DUMP filter you want to run on the remote hosts",
+        '--filter',
+        help="The tcpdump filter you want to run on the remote hosts",
+        required=True,
     )
 
     parser.add_argument(
@@ -163,6 +222,17 @@ def process_cli_arguments():
         '--username',
         help="Username when connecting to hosts",
         default=None,
+    )
+
+    parser.add_argument(
+        '--sudo',
+        help="Execute tcpdump with sudo, expects no password prompt",
+        action='store_true',
+    )
+
+    parser.add_argument(
+        '--pcap',
+        help="Instruct tcpdump to echo out in pcap format and store locally for analysis",
     )
 
     return parser.parse_args()
